d9317b6632a916b3696526bf9e289857d6a8e2bf
mickey1132 <mickylin1132@gmail.com>
2024-03-14 16:33:20 +0800

    Week04

--- week04/week04_rotate_translate_scale/main.cpp
+++ week04/week04_rotate_translate_scale/main.cpp
@@ -7,10 +7,10 @@ void display()
     glClearColor(1.0,1.0,0.9,1.0);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-        glTranslatef(0.8,0,0);
         glRotated(angle++,0,0,1);
+        glTranslatef(0.8,0,0);
         glScalef(0.3,0.3,0.3);
-        glColor3f(0,1,0)
+        glColor3f(0,1,0);
         glutSolidTeapot(0.3);
     glPopMatrix();
     glutSwapBuffers();
