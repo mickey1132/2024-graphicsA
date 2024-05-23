3e2a8bb08dc07cfb14feeaeb6af7fc908f6a0f20
mickey1132 <mickylin1132@gmail.com>
2024-05-23 14:18:52 +0800

    Week14

--- finalwork/main.cpp
+++ finalwork/main.cpp
@@ -1,5 +1,3 @@
-///先把 week08-1_glm_gundam 的程式貼上來
-///等一下, 要再加貼圖的 18行程式
 #include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
 #include <opencv/cv.h>
 #include <GL/glut.h>
@@ -20,154 +18,168 @@ int myTexture(char * filename)
 }
 #include <GL/glut.h>
 #include "glm.h"
-GLMmodel * pmodel = NULL;
-GLMmodel * handa = NULL;
-GLMmodel * handb = NULL;
-GLMmodel * upperA = NULL;
-GLMmodel * lowerA = NULL;
-GLMmodel * body = NULL;
+GLMmodel*pmodel =NULL;
+GLMmodel*HandA =NULL;
+GLMmodel*HandB =NULL;
+GLMmodel*upperA =NULL;
+GLMmodel*lowerA =NULL;
+GLMmodel*Body =NULL;
 void drawBody(void)
 {
-    if (!body) {
-	body = glmReadOBJ("data/body.obj");
-	if (!body) exit(0);
-	glmUnitize(body);
-	glmFacetNormals(body);
-	glmVertexNormals(body, 90.0);
+    if (!Body) {
+	Body = glmReadOBJ("data2/body.obj");
+	if (!Body) exit(0);
+	glmUnitize(Body);
+	glmFacetNormals(Body);
+	glmVertexNormals(Body, 90.0);
     }
 
-    glmDraw(body, GLM_SMOOTH | GLM_TEXTURE);
-}
-void drawmodel(void)
-{
-    if (!pmodel) {
-	pmodel = glmReadOBJ("data/Gundam.obj");
-	if (!pmodel) exit(0);
-	glmUnitize(pmodel);
-	glmFacetNormals(pmodel);
-	glmVertexNormals(pmodel, 90.0);
-    }
-
-    glmDraw(pmodel, GLM_SMOOTH | GLM_TEXTURE);
+    glmDraw(Body, GLM_SMOOTH | GLM_MATERIAL);
 }
 void drawupperA(void)
 {
     if (!upperA) {
-	upperA = glmReadOBJ("data/upperA.obj");
+	upperA = glmReadOBJ("data2/upperA.obj");
 	if (!upperA) exit(0);
 	glmUnitize(upperA);
 	glmFacetNormals(upperA);
 	glmVertexNormals(upperA, 90.0);
     }
 
-    glmDraw(upperA, GLM_SMOOTH | GLM_TEXTURE);
+    glmDraw(upperA, GLM_SMOOTH | GLM_MATERIAL);
 }
 void drawlowerA(void)
 {
     if (!lowerA) {
-	lowerA = glmReadOBJ("data/lowerA.obj");
+	lowerA = glmReadOBJ("data2/lowerA.obj");
 	if (!lowerA) exit(0);
 	glmUnitize(lowerA);
 	glmFacetNormals(lowerA);
 	glmVertexNormals(lowerA, 90.0);
     }
 
-    glmDraw(lowerA, GLM_SMOOTH | GLM_TEXTURE);
+    glmDraw(lowerA, GLM_SMOOTH | GLM_MATERIAL);
 }
- void myBody(){///我的身體
-    glPushMatrix();
-        glColor3f(1, 0, 0);///紅色的
-        glutSolidCube(0.6);///myBody()
-    glPopMatrix();
+void drawHandA(void)
+{
+    if (!HandA) {
+	HandA = glmReadOBJ("data2/HandA.obj");
+	if (!HandA) exit(0);
+	glmUnitize(HandA);
+	glmFacetNormals(HandA);
+	glmVertexNormals(HandA, 90.0);
+    }
+
+    glmDraw(HandA, GLM_SMOOTH | GLM_MATERIAL);
 }
-///float angle = 0, da = 1; ///加這行, 讓它轉動
-float angle[20] = {};
-int angleID = 0;
-int oldX = 0, oldY = 0;
+void drawHandB(void)
+{
+    if (!HandB) {
+	HandB = glmReadOBJ("data2/HandB.obj");
+	if (!HandB) exit(0);
+	glmUnitize(HandB);
+	glmFacetNormals(HandB);
+	glmVertexNormals(HandB, 90.0);
+    }
+
+    glmDraw(HandB, GLM_SMOOTH | GLM_MATERIAL);
+}
 #include <stdio.h>
-FILE * fin = NULL;
-FILE * fout = NULL;
-void motion(int x, int y){ ///加入mouse motion 對應的函式
-    angle[angleID] += y - oldY; /// 當我們的 mouse motion 時, y座標的對應角度
-    oldX = x;
-    oldY = y;
+void myBody()
+{
+    glPushMatrix();
+        glColor3f(1,0,0);
+        glutSolidCube(0.6);///Body
+	glPopMatrix();
+}
+///float angle=0, da=1;
+float angle[20]={};
+int angleID=0;
+int oldX=0,oldY=0;
+FILE * fin=NULL;
+FILE * fout=NULL;
+void motion(int x,int y)
+{
+    angle[angleID]+=y-oldY;
+    oldX=x;
+    oldY=y;
     glutPostRedisplay();
-    if(fout==NULL) fout = fopen("angle.txt", "w+");
-    for(int i=0; i<20; i++){
-        printf("%.1f ", angle[i] );
-        fprintf(fout, "%.1f ", angle[i] );
+    if(fout==NULL) fout=fopen("angle.txt","w+");
+    for(int i=0;i<20;i++){
+        printf("%.1f ",angle[i]);
+        fprintf(fout,"%.1f ",angle[i]);
     }
     printf("\n");
-    fprintf(fout, "\n");
+    fprintf(fout,"\n");
 }
-
-void mouse(int button, int state, int x, int y) {
-    oldX = x;
-    oldY = y;
+void mouse(int botton,int state,int x,int y)
+{
+    oldX=x;
+    oldY=y;
 }
-
-void keyboard(unsigned char key, int x, int y) {
-	if(key=='r') {
-		if(fin==NULL) fin = fopen("angle.txt", "r");
-		for(int i=0; i<20; i++){
-			fscanf(fin, "%f", & angle[i] );
-		}
-		glutPostRedisplay();
-	}
-    if(key=='0') angleID = 0;
-    if(key=='1') angleID = 1;
-    if(key=='2') angleID = 2;
-    if(key=='3') angleID = 3;
+void keyboard(unsigned char key, int x ,int y)
+{
+    if(key=='0') angleID=0;
+    if(key=='1') angleID=1;
+    if(key=='2') angleID=2;
+    if(key=='3') angleID=3;
+    if(key=='r' or key=='R'){
+        if(fin==NULL)fin=fopen("angle.txt","r");
+        for(int i=0;i<20;i++){
+            fscanf(fin,"%f",&angle[i]);
+        }
+        glutPostRedisplay();
+    }
 }
 void display()
 {
+
+    glClearColor(0.3,0.3,0.3,0);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    ///glDisable(GL_TEXTURE_2D);
+    glDisable(GL_TEXTURE_2D);
 
     glPushMatrix();
-        glRotatef(angle[0], 0, 1, 0);
-        drawBody(); ///這裡
-        ///glutSolidSphere(0.1, 30, 30); ///加一個圓球體代表轉動中心
+        glRotatef(angle[0],0,1,0);
+        drawBody();
         glEnable(GL_TEXTURE_2D);
-        glColor3f(1, 1, 1);
+        glColor3f(1,1,1);
 
         glPushMatrix();
-            glTranslatef(-0.1, 0, 0); ///這裡掛在肩上
-            glRotatef(angle[1], 0, 0, 1);
-            glRotatef(angle[2], 1, 0, 0);
-            glTranslatef(0, -0.07, 0);
+            glTranslatef(-0.215,0.08,0);
+            glRotatef(angle[1],0,0,1);
+            glRotatef(angle[2],1,0,0);
+            glTranslatef(0,-0.07,0);
             drawupperA();
-            glPushMatrix(); ///(0)
-                glTranslatef(-0.02, -0.09, 0); ///(3)再掛到上手臂的位置
-                glRotatef(angle[3], 1, 0, 0); ///(2)x軸轉
-                glTranslatef(0, -0.21, 0); ///(1)
+            glPushMatrix();
+                glScalef(0.8,0.8,0.8);
+                glTranslatef(-0.02,-0.23,0);
+                glRotatef(angle[3],0,0,1);
+                glTranslatef(0,-0.21,0);
                 drawlowerA();
-            glPopMatrix(); ///(0)
+            glPopMatrix();
         glPopMatrix();
-    glPopMatrix();
+
     glutSwapBuffers();
 }
-
 const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
 const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, -5.0f, 0.0f };///加這行, 讓它轉動
+const GLfloat light_position[] = { 2.0f, 5.0f, -5.0f, 0.0f };
 
 const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
 const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
 const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat high_shininess[] = { 100.0f };
-
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
-    glutCreateWindow("week13-1");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week13-1 gundam");
     glutDisplayFunc(display);
-    glutIdleFunc(display); ///加這行, 讓它轉動
-    glutMouseFunc(mouse); ///大象放到冰箱
-    glutMotionFunc(motion); ///滑鼠控制
-    glutKeyboardFunc(keyboard); ///week13-1新加
+    glutMouseFunc(mouse);
+    glutIdleFunc(display);
+    glutMotionFunc(motion);
+    glutKeyboardFunc(keyboard);
 
     myTexture("data/Diffuse.jpg");
 
