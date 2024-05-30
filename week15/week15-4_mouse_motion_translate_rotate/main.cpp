4cb2d0ee9e35c2fb27815a8705d3126a4b1e1ee3
mickey1132 <mickylin1132@gmail.com>
2024-05-30 16:25:24 +0800

    Week15

--- week15/week15-4_mouse_motion_translate_rotate/main.cpp
+++ week15/week15-4_mouse_motion_translate_rotate/main.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <stdio.h>
 float angleX[10]={},angleY[10]={};
 float teapotX=0,teapotY=0;
 void display()
@@ -20,6 +21,7 @@ void motion(int x,int y)
 {
     teapotX+=(x-oldX)/150.0;
     teapotY-=(y-oldY)/150.0;
+    printf("glTranslatef(%.3f,%.3f,0);\n",teapotX,teapotY);
     oldX=x;
     oldY=y;
     glutPostRedisplay();
